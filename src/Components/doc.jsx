fe5f81d7c7b084b27c845a03bd1801816c7851c6
Ararat Mirzoyan <araratmirzoyan2011@gamil.com>
2026-08-04 23:48:47 +0400

    uxxumner

--- src/Components/doc.jsx
+++ src/Components/doc.jsx
@@ -1,7 +1,7 @@
 export default function Doc({ inf }) {
     return (
-        <div className="flex bg-[whitesmoke] text-gray-500 h-[200px] items-center justify-center transition-transform duration-300 ease-in-out shadow-[0_4px_6px_rgba(0,0,0,0.1)] hover:scale-[1.01] hover:shadow-[0_20px_30px_rgba(0,0,0,0.2)] hover:text-red-500">
-            <i className="fa-regular fa-file-pdf fa-3x  "></i>
+        <div className="flex bg-[whitesmoke] text-gray-500 h-[200px] p-[30px]  transition-transform duration-300 ease-in-out shadow-[0_4px_6px_rgba(0,0,0,0.1)] hover:scale-[1.01] hover:shadow-[0_20px_30px_rgba(0,0,0,0.2)] hover:text-red-500">
+            <i className="fa-regular fa-file-pdf fa-4x  "></i>
             <p className="text-[20px] max-[660px]:text-[16px]">{inf}</p>
         </div>
     );
