d7cad06f5f447bbb1760dfca81026f602e1cdf8c
Ararat Mirzoyan <araratmirzoyan2011@gamil.com>
2026-08-20 21:11:31 +0400

    + 1 ej

--- src/Components/hborder3.jsx
+++ src/Components/hborder3.jsx
@@ -3,16 +3,21 @@ import { button } from "./button1";
 export function hborder3(vernagir, text) {
   return (
     <div
-      className="group h-[300px] flex flex-col justify-around items-center bg-[whitesmoke] text-[18px] text-gray-500 font-sans
-                 transition-all duration-300 shadow-md hover:scale-[1.01] hover:shadow-xl hover:text-black"
+      className="group flex flex-col justify-between bg-white rounded-2xl p-6 shadow-md 
+                 transition-all duration-300 hover:scale-[1.01] hover:shadow-xl border border-gray-100"
     >
-      <div className="w-full h-[20%] text-center text-gray-500 transition-all duration-1000 group-hover:bg-cyan-400 group-hover:text-black">
-        <h1 className="text-[32px] text-black">{vernagir}</h1>
+      <div>
+        <h2 className="text-[22px] font-bold text-gray-900 mb-3 group-hover:text-[#f0645a] transition-colors">
+          {vernagir}
+        </h2>
+        <p className="text-[14px] text-gray-600 leading-relaxed mb-6">
+          {text}
+        </p>
       </div>
 
-      <p className="text-[18px]">{text}</p>
-
-      {button()}
+      <div className="mt-auto">
+        {button()}
+      </div>
     </div>
   );
 }
--- src/pages/Entertainment.jsx
+++ src/pages/Entertainment.jsx
@@ -15,7 +15,6 @@ function Entertainment() {
             {header()}
             {bgimg("https://www.telecomarmenia.am/images/menu/1/17392713413788.png")}
             
-            {/* Վերին մենյուի կոճակները */}
             <div className="mt-[-50px] h-[120px] bg-white shadow-md rounded-lg flex justify-center items-center w-4/5 ml-[10%] max-[1200px]:w-[90%] max-[1200px]:ml-[5%] max-[900px]:w-[94%] max-[900px]:ml-[3%] z-10 relative">
                 <HBorder url="https://www.telecomarmenia.am/files/icons/1/16510708352906/45x45.png" text="TeamTV" onClickHandler={() => navigate('/team-tv')} />
                 <HBorder url="https://www.telecomarmenia.am/files/icons/1/16511222304797/45x45.png" text="Payment services" onClickHandler={() => navigate('/PaymentService')} />
@@ -26,10 +25,7 @@ function Entertainment() {
 
             <h1 className="text-[40px] ml-[10%] font-bold text-[rgb(22,20,20)] mt-[60px] mb-[40px]">Entertainment</h1>
             
-            {/* Հիմնական ցանցը (Grid) */}
             <div className="w-[80%] ml-[10%] mb-[150px] grid grid-cols-3 gap-5 max-xl:grid-cols-3 max-[800px]:grid-cols-2 max-[700px]:grid-cols-1">
-                
-                {/* col-span-2-ը ստիպում է, որ այս բանները զբաղեցնի 2 սյունակի լայնություն (ինչպես նկարում) */}
                 <BannerCard 
                     bgImage="https://www.telecomarmenia.am/images/menu/1/165106349279.png"
                     title="SMS Premium"
@@ -39,7 +35,6 @@ function Entertainment() {
                     className="col-span-2 max-xl:col-span-full"
                 />
 
-                {/* Մյուս քարտերը կշարվեն դրա կողքից և ներքևից */}
                 {hborder3(
                     "Balance transfer", 
                     "The service allows Team mobile network prepaid system subscribers to transfer money from own balance to the balance of another Team subscriber"
@@ -55,6 +50,29 @@ function Entertainment() {
                 {hborder3(
                     "Recharge my balance", 
                     "The service «Recharge my balance» gives an opportunity to mobile prepaid and postpaid subscribers of Team to send a request on money transfer to the balance..."
+                )}{hborder3(
+                    "Book Portal", 
+                    "The service gives an opportunity to read and listen on-line books of various genres, which are placed at book.telecomarmenia.am portal, as well as to download a book per..."
+                )}
+                {hborder3(
+                    "Dating portal", 
+                    "New acquaintances, interesting interlocutors and interactive communication are waiting for you at the absolutely new Dating portal"
+                )}
+                {hborder3(
+                    "Kids Portal", 
+                    "The service presents kids.telecomarmenia.am portal, where books, films, tutorial programs, cartoons, games for kids and their parents..."
+                )}
+                {hborder3(
+                    "Humor Club", 
+                    "«Humor Club» entertainment portal with the services “Alibi” and “Bluff” will help you create surprise for your friends, play tricks with your loved ones, or just cheer them up"
+                )}
+                {hborder3(
+                    "FunBox", 
+                    "You can use the following services: Acquaintances: create your own profile, change / delete it if you wish, and follow other users' profiles Player: listen to the..."
+                )}
+                {hborder3(
+                    "Laughcall", 
+                    "The “Laughcall” service allows you to make calls within Team mobile network, changing your voice in online mode. You are offered the following voice versions: Baby..."
                 )}
             </div>
 
